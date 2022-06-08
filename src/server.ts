8ed64a5dcb06a98d363c3a859c3c067ad4f92b82
SahilScalelot <devsahil.scalelot@gmail.com>
2022-06-08 15:42:41 +0530

    Demo Branch

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,6 @@ import {environment, port} from "./config/Config";
 
 const appWideConfig: AppWideConfig = container.resolve(AppWideConfig);
 
-
 appWideConfig.getConfiguredApp().listen(port, () => {
     Logger.info(`server running on port : ${port}`);
     Logger.info("App Running for profile: " + environment);
